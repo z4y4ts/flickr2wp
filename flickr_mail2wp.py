f0ae7fae201f2cfc6c3e5bf272f68bbdc3b06832
Alexander Zayats <alexander.zayats@gmail.com>
2014-04-06 02:30:16 +0300

    Proper logging
    Why does it have to be so verbose?

--- flickr_mail2wp.py
+++ flickr_mail2wp.py
@@ -8,7 +8,12 @@ from flickr2wp_worker import get_set_photos, render_photos
 
 FLICKR_SET_URL = r'https://www.flickr.com/.+/photos/(?P<user>\w+)/sets/(?P<set>\d+)/'
 
-logging.basicConfig(filename='/tmp/flickr2wordpress.log',level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('/tmp/flickr2wordpress.log')
+fh.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+logger.debug('Starting processing...')
 
 def get_user_set_from_email(raw_email):
     match = re.search(FLICKR_SET_URL, raw_email)
@@ -19,16 +24,16 @@ def get_user_set_from_email(raw_email):
         raise Exception("Can't find user & set in email text.")
 
 def main():
-    logging.debug('Message received')
+    logger.debug('Message received')
     content = sys.stdin.read()
     user_id, set_id = get_user_set_from_email(content)
     photos = get_set_photos(set_id)
-    logging.debug('Creating new draft post.')
-    logging.debug(render_photos(photos))
+    logger.debug('Creating new draft post.')
+    logger.debug(render_photos(photos))
     # create_new_post()
 
 
-logging.debug('Starting processing...')
+logger.debug('Starting processing...')
 if __name__ == '__main__':
     main()
 
